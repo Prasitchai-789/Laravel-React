81f147097ae83865f569f6e8317bfbb689b2d591
prasitchai-789 <prasitchai.f@gmail.com>
2025-10-08 10:36:32 +0700

    Purchase_NewProject

--- app/Models/WIN/PODT.php
+++ app/Models/WIN/PODT.php
@@ -10,11 +10,18 @@ class PODT extends Model
     protected $connection = 'sqlsrv2';
     use HasFactory;
     protected $table = 'PODT';
-    protected $fillable = [];
     protected $primaryKey = 'POID';
+    public $timestamps = false;
+    protected $fillable = [
+        'POID', 'ListNo', 'GoodID', 'GoodName', 'GoodQty2', 'GoodPrice2', 'GoodAmnt', 'DocuType', 'RefPOID'
+    ];
 
     public function header()
     {
         return $this->belongsTo(POHD::class, 'POID', 'POID');
+    }
+    public function good()
+    {
+        return $this->belongsTo(EMGood::class, 'GoodID', 'GoodID');
     }
 }
--- app/Models/WIN/POHD.php
+++ app/Models/WIN/POHD.php
@@ -10,11 +10,22 @@ class POHD extends Model
     protected $connection = 'sqlsrv2';
     use HasFactory;
     protected $table = 'POHD';
-    protected $fillable = [];
     protected $primaryKey = 'POID';
+    public $timestamps = false;
+    protected $fillable = [
+        'DocuDate', 'DocuNo', 'DeptID', 'ReqByID', 'DocuType', 'AppvDate'
+    ];
 
     public function details()
     {
         return $this->hasMany(PODT::class, 'POID', 'POID');
+    }
+    public function department()
+    {
+        return $this->belongsTo(EMDept::class, 'DeptID', 'DeptID');
+    }
+    public function requester()
+    {
+        // return $this->belongsTo(EMEmp::class, 'ReqByID', 'EmpID');
     }
 }
